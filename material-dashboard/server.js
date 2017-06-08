43d63e1df00c0973dcfb1258f436e01d3f5a3f59
g-laz77 <suhanprabhu@gmail.com>
2017-06-08 14:32:44 +0530

    Added initial node server with Dashboard

---
+++ material-dashboard/server.js
@@ -0,0 +1,190 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+//var formidable = require("formidable");
+var util = require('util');
+const spawn = require('child_process').spawn;
+// Create a server
+// http.createServer( function (request, response) {  
+//     if (request.method.toLowerCase() == 'get') {
+//         display(request,response);
+//     } else if (request.method.toLowerCase() == 'post') {
+//         processAllFieldsOfTheForm(request, response);
+//     }
+// }).listen(8019);
+
+http.createServer(function (req, res) {
+
+    if(req.url.indexOf('.html') != -1){ //req.url has the pathname, check if it conatins '.html'
+
+      fs.readFile(__dirname + '/examples/dashboard.html', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+
+    if(req.url.indexOf('.js') != -1){ //req.url has the pathname, check if it conatins '.js'
+
+      if(req.url.indexOf('/assets/js/material-dashboard.js') != -1){
+        fs.readFile(__dirname + '/assets/js/material-dashboard.js', function (err, data) {
+          if (err) console.log(err);
+          res.writeHead(200, {'Content-Type': 'text/javascript'});
+          res.write(data);
+          res.end();
+        });
+      }
+      if(req.url.indexOf('/assets/js/jquery-3.1.0.min.js') != -1){
+        fs.readFile(__dirname + '/assets/js/jquery-3.1.0.min.js', function (err, data) {
+          if (err) console.log(err);
+          res.writeHead(200, {'Content-Type': 'text/javascript'});
+          res.write(data);
+          res.end();
+        });
+      }
+      if(req.url.indexOf('/assets/js/bootstrap.min.js') != -1){
+        fs.readFile(__dirname + '/assets/js/bootstrap.min.js', function (err, data) {
+          if (err) console.log(err);
+          res.writeHead(200, {'Content-Type': 'text/javascript'});
+          res.write(data);
+          res.end();
+        });
+      }
+      if(req.url.indexOf('/assets/js/material.min.js') != -1){
+        fs.readFile(__dirname + '/assets/js/material.min.js', function (err, data) {
+          if (err) console.log(err);
+          res.writeHead(200, {'Content-Type': 'text/javascript'});
+          res.write(data);
+          res.end();
+        });
+      }
+      if(req.url.indexOf('/assets/js/chartist.min.js') != -1){
+        fs.readFile(__dirname + '/assets/js/chartist.min.js', function (err, data) {
+          if (err) console.log(err);
+          res.writeHead(200, {'Content-Type': 'text/javascript'});
+          res.write(data);
+          res.end();
+        });
+      }
+      if(req.url.indexOf('/assets/js/bootstrap-notify.js') != -1){
+        fs.readFile(__dirname + '/assets/js/bootstrap-notify.js', function (err, data) {
+          if (err) console.log(err);
+          res.writeHead(200, {'Content-Type': 'text/javascript'});
+          res.write(data);
+          res.end();
+        });
+      }
+      if(req.url.indexOf('/server.js') != -1){
+        fs.readFile(__dirname + '/server.js', function (err, data) {
+          if (err) console.log(err);
+          res.writeHead(200, {'Content-Type': 'text/javascript'});
+          res.write(data);
+          res.end();
+        });
+      }
+      if(req.url.indexOf('/assets/js/demo.js') != -1){
+        fs.readFile(__dirname + '/assets/js/demo.js', function (err, data) {
+          if (err) console.log(err);
+          res.writeHead(200, {'Content-Type': 'text/javascript'});
+          res.write(data);
+          res.end();
+        });
+      }
+
+    }
+
+    if(req.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
+      if(req.url.indexOf('/assets/css/material-dashboard.css') != -1){
+        fs.readFile(__dirname + '/assets/css/material-dashboard.css', function (err, data) {
+          if (err) console.log(err);
+          res.writeHead(200, {'Content-Type': 'text/css'});
+          res.write(data);
+          res.end();
+        });
+      }
+      if(req.url.indexOf('/assets/css/bootstrap.min.css') != -1){
+        fs.readFile(__dirname + '/assets/css/bootstrap.min.css', function (err, data) {
+          if (err) console.log(err);
+          res.writeHead(200, {'Content-Type': 'text/css'});
+          res.write(data);
+          res.end();
+        });
+      }
+      if(req.url.indexOf('/assets/css/demo.css') != -1){
+        fs.readFile(__dirname + '/assets/css/demo.css', function (err, data) {
+          if (err) console.log(err);
+          res.writeHead(200, {'Content-Type': 'text/css'});
+          res.write(data);
+          res.end();
+        });
+      }
+    }
+}).listen(1339);
+
+
+function display(request,response){
+    // Parse the request containing file name
+   var pathname = url.parse(request.url).pathname;
+   
+   // Print the name of the file for which request is made.
+   console.log("Request for " + pathname + " received.");
+   
+   // Read the requested file content from file system
+   fs.readFile(pathname.substr(1), function (err, data) {
+      if (err) {
+         console.log(err);
+         // HTTP Status: 404 : NOT FOUND
+         // Content Type: text/plain
+         response.writeHead(404, {'Content-Type': 'text/html'});
+      }else {	
+         //Page found	  
+         // HTTP Status: 200 : OK
+         // Content Type: text/plain
+         response.writeHead(200, {'Content-Type': 'text/html'});	
+         
+         // Write the content of the file to response body
+         response.write(data.toString());		
+      }
+      // Send the response body 
+      response.end();
+    });     
+}
+
+function processAllFieldsOfTheForm(req, res) {
+    var form = new formidable.IncomingForm();
+    console.log("kk");  
+    form.parse(req, function (err, fields, files) {
+        //Store the data from the fields in your data store.
+        //The data store could be a file or database or any other store based
+        //on your application.
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        res.write('\n\n');
+        console.log("whatup");
+        fs.writeFileSync("url.txt",fields.ss);
+        const execFile = require('child_process').execFile;
+        const spawn = require('child_process').spawn;
+        const ls = spawn('scrapy', ['crawl', 'mygov_spider']);
+
+        ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        });
+
+        ls.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+        });
+
+        ls.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+        var temp = fs.readFileSync("webCrawaler/details.txt");
+        res.end(temp);
+        //     fields: fields.ss,
+        //     files: files
+        // }));
+    });
+}
