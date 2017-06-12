5222ae5976113b118ed403e504bfecd22b8806b5
g-laz77 <suhanprabhu@gmail.com>
2017-06-12 11:54:43 +0530

    added the seo stats api

--- material-dashboard/server.js
+++ material-dashboard/server.js
@@ -195,11 +195,27 @@ function processAllFieldsOfTheForm(req, res) {
                 }
                 else if(name=="Tweets")
                   fs.writeFileSync("tweets.txt",number);
-                //array.push(metadata)
                 
             });
-            //console.log(array);
-            //fs.writeFileSync("social_media.txt",array);
+          var $ = cheerio.load(body);
+            $('div.container').each(function(i, element){
+                var number = $(element).children().children().eq(0).children().eq(1);
+                if(number)
+                {
+                    $(number).each(function(j,elem){
+                        if($(elem).text() === "SEO Stats"){
+                            var gip = $(elem).parent().next().children().children().children().children().children().eq(1).children().children().eq(1).text();
+                            var yip = $(elem).parent().next().children().children().children().children().children().eq(2).children().children().eq(1).text();
+                            var bip = $(elem).parent().next().children().children().children().children().children().eq(3).children().children().eq(1).text();
+                            fs.appendFileSync('seo.txt', gip);
+                            fs.appendFileSync('seo.txt', "\n");
+                            fs.appendFileSync('seo.txt', yip);
+                            fs.appendFileSync('seo.txt', "\n");
+                            fs.appendFileSync('seo.txt', bip);                                
+                        }
+                    });
+                }
+            });
         });
         console.log("Form input submitted");
         //res.end()
--- sample.js
+++ sample.js
@@ -1,22 +1,29 @@
 var request = require('request');
 var cheerio = require('cheerio');
-
+var fs = require('fs');
 
 request('http://www.valbot.com/google.com', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred 
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
   var array = [];
   var $ = cheerio.load(body);
-    $('div.panel').each(function(i, element){
-        //console.log(this)
-        var number = $(element).children().first().next().children().eq(0).text()
-        var name = $(element).children().first().next().children().eq(1).text()
-        var metadata = {
-            Type: name,
-            Number: number,
-        };
-        array.push(metadata)
-        
+    $('div.container').each(function(i, element){
+        var number = $(element).children().children().eq(0).children().eq(1);
+        if(number)
+        {
+            $(number).each(function(j,elem){
+                if($(elem).text() === "SEO Stats"){
+                    var gip = $(elem).parent().next().children().children().children().children().children().eq(1).children().children().eq(1).text();
+                    var yip = $(elem).parent().next().children().children().children().children().children().eq(2).children().children().eq(1).text();
+                    var bip = $(elem).parent().next().children().children().children().children().children().eq(3).children().children().eq(1).text();
+                    fs.appendFileSync('seo.txt', gip);
+                    fs.appendFileSync('seo.txt', "\n");
+                    fs.appendFileSync('seo.txt', yip);
+                    fs.appendFileSync('seo.txt', "\n");
+                    fs.appendFileSync('seo.txt', bip);                                
+                }
+            });
+        }
     });
     console.log(array);
 });
